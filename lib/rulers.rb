b99e8e17d22897217db5de54d7909c0791892207
Jason Bornhoft <jmvbxx@gmail.com>
2022-01-05 10:26:39 -0500

    Chapter two

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,13 +3,26 @@
 require_relative "rulers/version"
 require "rulers/array"
 require "rulers/average_two_numbers"
+require "rulers/routing"
 
 module Rulers
   # The main application class (for now, I guess)
   class Application
     def call(_env)
-      `echo debug > debug.txt`
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, { "Content-Type" => "text/html" }, ["Hello from Ruby on Rulers!"]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
