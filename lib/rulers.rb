27971d9942ffb861e0ef115e51ff7586727c5da6
Jason Bornhoft <jmvbxx@gmail.com>
2022-01-04 22:07:33 -0500

    Chapter one

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require "rulers/array"
+require "rulers/average_two_numbers"
 
 module Rulers
-  class Error < StandardError; end
-  # Your code goes here...
+  # The main application class (for now, I guess)
+  class Application
+    def call(_env)
+      `echo debug > debug.txt`
+      [200, { "Content-Type" => "text/html" }, ["Hello from Ruby on Rulers!"]]
+    end
+  end
 end
