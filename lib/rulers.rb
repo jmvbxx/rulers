27730abe1c240d4f666ae70b5a81179f9b675527
Jason Bornhoft <jmvbxx@gmail.com>
2022-01-24 20:43:07 -0500

    Chapter six

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -24,7 +24,12 @@ module Rulers
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-        [200, { "Content-Type" => "text/html" }, [text]]
+        r = controller.get_response
+        if r
+          [r.status, r.headers, [r.body].flatten]
+        else
+          [200, { "Content-Type" => "text/html" }, [text]]
+        end
       end
     end
   end
